44f8a2a46acc55dcfd37034d5639d817029976d0
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-09-24 00:00:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
+Car.prototype.getMakeModel = function(){
+		return `${this.make} ${this.model}`;
+};
 
 function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	this.make = make;
+	this.model = model;
+	this.topSpeed = topSpeed;
+
+}
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
-window.SportsCar = SportsCar;
